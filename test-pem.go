6a86d0c5c4149ae1f5df18c0d8c41374bcb74b2f
Monica Chew <mmc@mozilla.com>
2014-05-29 11:46:52 -0700

    fmt, package

--- test-pem.go
+++ test-pem.go
@@ -1,4 +1,4 @@
-package main
+package certificatetransparency
 
 import "fmt"
 import "crypto/x509"
@@ -10,35 +10,35 @@ import "bufio"
 import "os"
 
 func main() {
-        pemFile, err := os.Open(os.Args[1])
-        scanner := bufio.NewScanner(bufio.NewReader(pemFile))
-        var pemCert []byte
-        for scanner.Scan() {
-                pemCert = append(pemCert, scanner.Bytes()...)
-                pemCert = append(pemCert, '\n')
-        }
-        fmt.Printf("%s", pemCert);
-        block, _ := pem.Decode(pemCert)
-        if block == nil {
-                fmt.Printf("Can't decode pem\n")
-                os.Exit(1)
+	pemFile, err := os.Open(os.Args[1])
+	scanner := bufio.NewScanner(bufio.NewReader(pemFile))
+	var pemCert []byte
+	for scanner.Scan() {
+		pemCert = append(pemCert, scanner.Bytes()...)
+		pemCert = append(pemCert, '\n')
+	}
+	fmt.Printf("%s", pemCert)
+	block, _ := pem.Decode(pemCert)
+	if block == nil {
+		fmt.Printf("Can't decode pem\n")
+		os.Exit(1)
 
-        }
-        if block.Bytes == nil {
-                fmt.Printf("No bytes\n")
-                os.Exit(1)
-        }
-        cert, err := x509.ParseCertificate(block.Bytes)
-        if err != nil {
-                fmt.Printf("Can't parse cert\n")
-                os.Exit(1)
-        }
-        hasher := sha1.New()
-        sha256hasher := sha256.New()
-        hasher.Write(cert.RawSubjectPublicKeyInfo)
-        sha256hasher.Write(cert.RawSubjectPublicKeyInfo)
-        fmt.Printf("Common name: %s\n", cert.Subject.CommonName)
-        fmt.Printf("Issuer name: %s\n", cert.Issuer.CommonName)
-        fmt.Printf("sha1/%s\n", base64.StdEncoding.EncodeToString(hasher.Sum(nil)));
-        fmt.Printf("sha256/%s\n", base64.StdEncoding.EncodeToString(sha256hasher.Sum(nil)));
+	}
+	if block.Bytes == nil {
+		fmt.Printf("No bytes\n")
+		os.Exit(1)
+	}
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		fmt.Printf("Can't parse cert\n")
+		os.Exit(1)
+	}
+	hasher := sha1.New()
+	sha256hasher := sha256.New()
+	hasher.Write(cert.RawSubjectPublicKeyInfo)
+	sha256hasher.Write(cert.RawSubjectPublicKeyInfo)
+	fmt.Printf("Common name: %s\n", cert.Subject.CommonName)
+	fmt.Printf("Issuer name: %s\n", cert.Issuer.CommonName)
+	fmt.Printf("sha1/%s\n", base64.StdEncoding.EncodeToString(hasher.Sum(nil)))
+	fmt.Printf("sha256/%s\n", base64.StdEncoding.EncodeToString(sha256hasher.Sum(nil)))
 }
